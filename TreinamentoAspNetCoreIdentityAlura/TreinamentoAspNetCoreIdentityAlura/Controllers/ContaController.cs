e089a8c21f0294f351cd9981846d8847dd64ca1c
Gil Costa <gil-real@hotmail.com>
2018-04-08 22:44:00 -0300

    criado a classe startup e configurado o Owin

--- TreinamentoAspNetCoreIdentityAlura/TreinamentoAspNetCoreIdentityAlura/Controllers/ContaController.cs
+++ TreinamentoAspNetCoreIdentityAlura/TreinamentoAspNetCoreIdentityAlura/Controllers/ContaController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.Threading.Tasks;
+using System.Web;
 using System.Web.Mvc;
 using TreinamentoAspNetCoreIdentityAlura.Models;
 using TreinamentoAspNetCoreIdentityAlura.ViewModels;
@@ -8,6 +11,27 @@ namespace TreinamentoAspNetCoreIdentityAlura.Controllers
 {
     public class ContaController : Controller
     {
+
+
+        private UserManager<UsuarioAplicacao> _userManager;
+
+        public UserManager<UsuarioAplicacao> UserManager
+        {
+            get
+            {
+                if(_userManager == null)
+                {
+                    var contextoOwin = HttpContext.GetOwinContext();
+                    _userManager = contextoOwin.GetUserManager<UserManager<UsuarioAplicacao>>();
+                }
+                return _userManager;
+            }
+            set
+            {
+                _userManager = value;
+            }
+        }
+
         // GET: Conta
         public ActionResult Registrar()
         {
@@ -15,17 +39,14 @@ namespace TreinamentoAspNetCoreIdentityAlura.Controllers
         }
         
         [HttpPost]
-        public ActionResult Registrar(ContaViewModel conta)
+        public async Task<ActionResult> Registrar(ContaViewModel conta)
         {
             if(ModelState.IsValid)
             {
-
                 //adiciona o usuario no banco
-                var _context = new IdentityDbContext<UsuarioAplicacao>("TreinamentoAspNetCoreIdentityConnection");
-
-                var userStore = new UserStore<UsuarioAplicacao>(_context);
-
-                var userManager = new UserManager<UsuarioAplicacao>(userStore);
+                //var _context = new IdentityDbContext<UsuarioAplicacao>("TreinamentoAspNetCoreIdentityConnection");
+                //var userStore = new UserStore<UsuarioAplicacao>(_context);
+                //var userManager = new UserManager<UsuarioAplicacao>(userStore);
             
 
                 var novoUsuario = new UsuarioAplicacao();
@@ -34,12 +55,9 @@ namespace TreinamentoAspNetCoreIdentityAlura.Controllers
                 novoUsuario.UserName = conta.NomeUsuario;
                 novoUsuario.NomeCompleto = conta.NomeCompleto;
 
-                _context.Users.Add(novoUsuario);
-                _context.SaveChanges();
-
-
-
-
+                await UserManager.CreateAsync(novoUsuario, conta.Senha);
+                //_context.Users.Add(novoUsuario);
+                //_context.SaveChanges();
 
                 // redireciona para pagina inicial
                 return RedirectToAction("index", "home");
---
+++ TreinamentoAspNetCoreIdentityAlura/TreinamentoAspNetCoreIdentityAlura/Startup.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Owin;
+using System.Data.Entity;
+using TreinamentoAspNetCoreIdentityAlura.Models;
+
+[assembly: OwinStartup(typeof(TreinamentoAspNetCoreIdentityAlura.Startup))]
+namespace TreinamentoAspNetCoreIdentityAlura
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder builder)
+        {
+            builder.CreatePerOwinContext<DbContext>(() =>
+            new IdentityDbContext<UsuarioAplicacao>("TreinamentoAspNetCoreIdentityConnection"));
+
+            builder.CreatePerOwinContext<IUserStore<UsuarioAplicacao>>(
+                (opcoes, contextoOwin) =>
+                {
+                    var _context = contextoOwin.Get<DbContext>();
+                    return new UserStore<UsuarioAplicacao>(_context);
+                });
+
+            builder.CreatePerOwinContext<UserManager<UsuarioAplicacao>>(
+                (opcoes, contextoOwin) =>
+                {
+                    var userStore = contextoOwin.Get <IUserStore<UsuarioAplicacao>>();
+                    return new UserManager<UsuarioAplicacao>(userStore);
+                });
+        }
+    }
+}
